e0f6ff2b74c08718f1209597aa0bba5b5b2da00d
lopesdasilva <lopesdasilva.spam@gmail.com>
2011-11-02 23:51:28 +0000

    CHange db address

--- src/java/users/userManager.java
+++ src/java/users/userManager.java
@@ -13,8 +13,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import sha1.sha1;
 
 /**
