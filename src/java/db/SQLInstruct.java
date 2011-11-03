a641e3bdba3f6f14ab1ae7c86c6f3f72044248de
lopesdasilva <lopesdasilva.spam@gmail.com>
2011-11-03 12:49:38 +0000

    end da bd para o servidor

--- src/java/db/SQLInstruct.java
+++ src/java/db/SQLInstruct.java
@@ -4,14 +4,14 @@ public class SQLInstruct {
 
     //Database Configs:
     //public static final String dbAdress="jdbc:mysql://192.168.1.2:3306/school";
-    //public static final String dbAdress="jdbc:mysql://lopesdasilva.servebeer.com:3306/school";
-    public static final String dbAdress="jdbc:mysql://localhost:3306/school";
+    public static final String dbAdress="jdbc:mysql://lopesdasilva.servebeer.com:3306/school";
+    //public static final String dbAdress="jdbc:mysql://localhost:3306/school";
     public static final String dbUsername="root";
-    public static final String dbPassword="";
+    public static final String dbPassword="rr2011";
    
 
     public static String login(String userName, String hashPassword) {
-        return "SELECT * FROM Users where"
+        return "SELECT * FROM User where"
                 + " username='" + userName + "' and password='" + hashPassword + "'";
     }
     public static String disciplinas (String username){
