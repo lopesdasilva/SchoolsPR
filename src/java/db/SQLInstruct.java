453f4fa95ec12a4f830ba17069b459df846a1c26
lopesdasilva <lopesdasilva.spam@gmail.com>
2011-11-03 18:15:57 +0000

    Revert c22ac5dad072bc514498663e43514cfee0abed6a^..HEAD

--- src/java/db/SQLInstruct.java
+++ src/java/db/SQLInstruct.java
@@ -3,8 +3,8 @@ package db;
 public class SQLInstruct {
 
     //Database Configs:
-    //public static final String dbAdress="jdbc:mysql://192.168.1.2:3306/school";
-    public static final String dbAdress="jdbc:mysql://lopesdasilva.servebeer.com:3306/school";
+    public static final String dbAdress="jdbc:mysql://192.168.1.2:3306/school";
+   // public static final String dbAdress="jdbc:mysql://lopesdasilva.servebeer.com:3306/school";
     //public static final String dbAdress="jdbc:mysql://localhost:3306/school";
     public static final String dbUsername="root";
     public static final String dbPassword="rr2011";
