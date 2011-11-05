9380a83ba3bf9097166f075663c156b8a4fa7c00
lopesdasilva <lopesdasilva.spam@gmail.com>
2011-11-05 13:22:16 +0000

    Implementacao do martelo

--- src/java/menu/MenuBean.java
+++ src/java/menu/MenuBean.java
@@ -1,7 +1,10 @@
 package menu;
 
 import java.util.LinkedList;
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
 import javax.el.MethodExpression;
+import javax.el.ValueExpression;
 import javax.faces.application.Application;
 import javax.faces.context.FacesContext;
 import javax.faces.el.MethodBinding;
@@ -37,11 +40,12 @@ public class MenuBean {
 
             MenuItem item = new MenuItem();
             item.setValue("Informações");
-            //item.setUrl("profile.xhtml")
-            Application app = FacesContext.getCurrentInstance().getApplication();
-            javax.faces.el.MethodBinding mb = app.createMethodBinding("#{user.onItemClick}", new Class[]{ActionEvent.class});
+            //item.setUrl("#");
 
-            item.setActionListener(mb);
+
+            MethodExpression methodExpression = FacesContext.getCurrentInstance().getApplication().getExpressionFactory().createMethodExpression(FacesContext.getCurrentInstance().getELContext(), "#{user.moduleSelection}", null, new Class[]{ActionEvent.class});
+            item.setAjax(false);
+            item.setActionExpression(createAction_old("#{user.yahoo}", String.class));
             //  item.setActionListener(createActionListener(("#{user.yahoo}")));
             submenu.getChildren().add(item);
 
@@ -49,11 +53,19 @@ public class MenuBean {
                 item = new MenuItem();
                 item.setValue(m.getName());
                 item.setAjax(false);
-                item.setValue(m.getName());
-                item.setActionListener(createActionListener("#{user.onItemClick}"));
+                
+                //item.setActionListener(createActionListener("#{user.moduleSelection}"));
 
                 //para usar mais tarde
-                //item.setActionExpression(createAction("#{user.yahoo}", ActionEvent.class));
+                // item.setActionExpression(createAction("#{user.yahoo}", String.class ,String.class));
+                //   String[] names={d.getName()};
+                //Class[] sadsa= {String.class};
+                //System.out.println("AQUIIIII"+resloveMethodExpression("#{user.moduleSelection}", null,new Class[] {String.class},null).toString());
+                // item.setActionExpression((MethodExpression)resloveMethodExpression("#{user.moduleSelection}", null,new Class[] {String.class},null));
+              
+                //MARTELO!!!
+                item.setActionListener(createActionListener("#{user.moduleSelection}"));
+                item.setActionExpression(createAction_old("#{user.yahoo}", String.class));
                 submenu.getChildren().add(item);
             }
             model.addSubmenu(submenu);
@@ -65,14 +77,34 @@ public class MenuBean {
         return model;
     }
 
-    public static MethodExpression createAction(String actionExpression, Class<?> returnType) {
+    public static Object resloveMethodExpression(String expression,
+            Class returnType,
+            Class[] argTypes,
+            Object[] argValues) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Application app = facesContext.getApplication();
+        ExpressionFactory elFactory = app.getExpressionFactory();
+        ELContext elContext = facesContext.getELContext();
+        MethodExpression methodExpression =
+                elFactory.createMethodExpression(elContext, expression, returnType,
+                argTypes);
+        return methodExpression.invoke(elContext, argValues);
+    }
+
+    public static MethodExpression createAction(String actionExpression, Class<?> returnType, Class<?> argType) {//,Class<?> argType) {
         FacesContext context = FacesContext.getCurrentInstance();
-        return context.getApplication().getExpressionFactory().createMethodExpression(context.getELContext(), actionExpression, returnType, new Class[0]);
+        return context.getApplication().getExpressionFactory().createMethodExpression(context.getELContext(), actionExpression, returnType, new Class[]{argType});
+    }
+
+    public static MethodExpression createAction_old(String actionExpression, Class<?> returnType) {//,Class<?> argType) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        return context.getApplication().getExpressionFactory().createMethodExpression(context.getELContext(), actionExpression, null, new Class[0]);
     }
 
     public static MethodBinding createActionListener(String actionListenerExpression) {
         Application app = FacesContext.getCurrentInstance().getApplication();
-        return app.createMethodBinding("#{user.onItemClick}", new Class[]{ActionEvent.class});
- 
+
+        return app.createMethodBinding(actionListenerExpression, new Class[]{ActionEvent.class});
+
     }
 }
--- src/java/users/userManager.java
+++ src/java/users/userManager.java
@@ -14,7 +14,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import menu.MenuBean;
 import org.primefaces.component.menuitem.MenuItem;
 import org.primefaces.component.submenu.Submenu;
 import sha1.sha1;
@@ -33,6 +32,36 @@ public class userManager {
     boolean loggedIn = false;
     String d = "teste d";
     User current;
+    String disciplineSelected;
+    String moduleSelected;
+    //Testes do moduleSeleccionado
+    String[] tests= {"","",""};
+    
+    
+    
+    String teste1="teste 1";
+
+    public String getTeste1() {
+        return teste1;
+    }
+
+    public String getTeste2() {
+        return teste2;
+    }
+
+    public String getTeste3() {
+        return teste3;
+    }
+    String teste2="Teste 2";
+    String teste3="Teste 3";
+
+    public String getDisciplineSelected() {
+        return disciplineSelected;
+    }
+
+    public String getModuleSelected() {
+        return moduleSelected;
+    }
 
     /** Creates a new instance of loginController */
     public userManager() {
@@ -108,40 +137,53 @@ public class userManager {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Wrong User or Password"));
         return "fail";
     }
-    
 
-    
+    public String yahoo() {
 
-    public String yahoo() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-        DBConnect db = new DBConnect(SQLInstruct.dbAdress, SQLInstruct.dbUsername, SQLInstruct.dbPassword);
-        db.loadDriver();
-
-        //Meter as variaveis que o rui ainda vai arranjar.
-        String sqlStatement = SQLInstruct.tests("Math", "SumModule");
-        ResultSet rSet = db.queryDB(sqlStatement);
-
-        while (rSet.next()) {
-            for (Discipline d : current.disciplines) {
-                if (d.getName().equals("Math")) {//Sybstituir Math
-                    //fazer while do rSet(Fazer rSet)
-                    d.addTest("SumModule", rSet.getString(1));//Substituir SumModule
-                }
-            }
-        }
         return "success";
     }
 
-    public void onItemClick(ActionEvent event) {
+    public String moduleSelection(ActionEvent event) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
         Object obj = event.getSource();
         MenuItem aux_module = (MenuItem) obj;
         Submenu aux_discipline = (Submenu) aux_module.getParent();
 
-        String moduleSelected = aux_module.getValue() + "";
-        String disciplineSelected = aux_discipline.getLabel();
+        moduleSelected = aux_module.getValue() + "";
+        disciplineSelected = aux_discipline.getLabel();
 
 
-        System.out.println("Module Selected: " + moduleSelected);
-        System.out.println("Discipline Selected: " + disciplineSelected);
+        //System.out.println("Module Selected: " + moduleSelected);
+        //System.out.println("Discipline Selected: " + disciplineSelected);
 
+
+        DBConnect db = new DBConnect(SQLInstruct.dbAdress, SQLInstruct.dbUsername, SQLInstruct.dbPassword);
+        db.loadDriver();
+
+        //Meter as variaveis que o rui ainda vai arranjar.
+        String sqlStatement = SQLInstruct.tests(disciplineSelected, moduleSelected);
+        ResultSet rSet = db.queryDB(sqlStatement);
+
+        while (rSet.next()) {
+            int i = 0;
+            for (Discipline d : current.disciplines) {
+                if (d.getName().equals("Math")) {//Sybstituir Math
+                    //fazer while do rSet(Fazer rSet)
+                        d.addTest("SumModule", rSet.getString(1));//Substituir SumModule
+                    tests[i] = rSet.getString(1);
+                    i++;
+                }
+
+            }
+
+        }
+//martelo !!
+       teste1=tests[0];
+        teste2=tests[1];
+        teste3=tests[2];
+
+
+
+
+        return "success";
     }
 }
