d901eb7a7024bf559e65195f6e68ce40644d8c5b
lopesdasilva <lopesdasilva.spam@gmail.com>
2011-11-21 10:03:41 +0000

    geri os conflitos.. a bd nao esta a 100%

--- src/java/db/SQLInstruct.java
+++ src/java/db/SQLInstruct.java
@@ -3,20 +3,13 @@ package db;
 public class SQLInstruct {
 
     //Database Configs:
-<<<<<<< HEAD
-    //public static final String dbAdress="jdbc:mysql://192.168.1.2:3306/school";
-    //public static final String dbAdress="jdbc:mysql://lopesdasilva.servebeer.com:3306/school";
-    public static final String dbAdress="jdbc:mysql://localhost:3306/School";
-    public static final String dbUsername="root";
-    public static final String dbPassword="";
-=======
+
     public static final String dbAdress="jdbc:mysql://www.freesql.org:3306/school";
    //public static final String dbAdress="jdbc:mysql://lopesdasilva.servebeer.com:3306/school";
     //public static final String dbAdress="jdbc:mysql://localhost:3306/school";
     public static final String dbUsername="schoolspr";
     public static final String dbPassword="rr2011";
->>>>>>> novo server sql
-   
+
 
     public static String login(String userName, String hashPassword) {
         return "SELECT * FROM user where"
