42b4e7164343239332c6e91ebcae5938bedda8fe
lopesdasilva <lopesdasilva.spam@gmail.com>
2011-12-06 23:37:23 +0000

    Sync

--- src/java/menu/MenuBean.java
+++ src/java/menu/MenuBean.java
@@ -20,10 +20,6 @@ import tables.Module;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-/**
- *
- * @author lopesdasilva
- */
 public class MenuBean implements Serializable{
 
     private MenuModel model;
